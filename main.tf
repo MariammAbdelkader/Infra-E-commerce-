edbd4f9c5c4f833eb2d677b6594f6937b48427a0
MariammAbdelkader <m.mohamed112002@gmail.com>
2024-11-22 13:48:18 +0200

    Adding public subnets

--- main.tf
+++ main.tf
@@ -12,5 +12,29 @@ resource "aws_vpc" "main" {
 }
 
 # Define Public Subnets 
+resource "aws_subnet" "public_subnet_a" {
+vpc_id            = aws_vpc.main.id 
+cidr_block              = "10.0.1.0/24"
+availability_zone       = "us-east-1a"
+tags = {
+    Name = "public-subnet-a"
+  }
+}
 
+resource "aws_subnet" "public_subnet_b" {
+vpc_id            = aws_vpc.main.id 
+cidr_block              = "10.0.2.0/24"
+availability_zone       = "us-east-1b"
+tags = {
+    Name = "public-subnet-b"
+  }
+}
 
+resource "aws_subnet" "public_subnet_c" {
+vpc_id            = aws_vpc.main.id 
+cidr_block              = "10.0.3.0/24"
+availability_zone       = "us-east-1c"
+  tags = {
+    Name = "public-subnet-c"
+  }
+}

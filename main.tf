72bd2e13db02a6b97af430d4821343ade16eeeb1
MariammAbdelkader <m.mohamed112002@gmail.com>
2024-11-23 09:08:31 +0200

    Internet Gateway and Public Route Table

--- main.tf
+++ main.tf
@@ -74,3 +74,26 @@ resource "aws_subnet" "private_subnet_c" {
   }
 }
 
+# Define Internet Gateway
+resource "aws_internet_gateway" "main" { 
+  vpc_id = aws_vpc.main.id 
+  tags = { 
+    Name = "main-gateway" 
+  } 
+}
+
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
